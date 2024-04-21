aa67601af40a3e15728ef9f46bba4dbd8ddc6987
Naga Durga Prasad <durgaprasadachana1108@gmail.com>
2024-04-22 04:42:54 +0530

    update in schema

---
+++ server/prisma/migrations/20240421224110_timestamps/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "createdAt";
