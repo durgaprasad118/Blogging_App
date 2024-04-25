2e98827269aedf0bce3946475dae996eb81607e4
Naga Durga Prasad <durgaprasadachana1108@gmail.com>
2024-04-25 09:38:13 +0530

    added code snippet

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -5,7 +5,7 @@ export interface EditorI {
 	setContent: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const apiKey = import.meta.env.VITE_KEY;
+const apiKey = "mu2fiadzwozcmwmerrtl4ew3rre5qk2kw1mya51xoldre8ju";
 
 function Editorr({ content, setContent }: EditorI) {
 	const onEditorInputChange = (newValue: string) => {
@@ -20,9 +20,9 @@ function Editorr({ content, setContent }: EditorI) {
 					direction: "ltr",
 					icons: "jam",
 					plugins:
-						"anchor autolink charmap codesample emoticons  link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter  linkchecker         tableofcontents footnotes mergetags autocorrect typography inlinecss markdown",
+						"anchor autolink charmap codesample emoticons  link lists media searchreplace table visualblocks wordcount checklist mediaembed code codesample casechange export formatpainter  linkchecker         tableofcontents footnotes mergetags autocorrect typography inlinecss markdown",
 					toolbar:
-						"undo redo | blocks fontfamily  | bold italic underline strikethrough | link   table  | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+						"undo redo | blocks fontfamily  | bold italic underline strikethrough | link   table  | addcomment showcomments codesample    | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
 					tinycomments_mode: "embedded",
 					tinycomments_author: "Author name",
 					mergetags_list: [
