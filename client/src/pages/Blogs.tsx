f870c6a8dd2540f5ffe0479b4396ec08a00f88e4
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-09-12 16:28:48 +0530

    Fix: frontend

--- client/src/pages/Blogs.tsx
+++ client/src/pages/Blogs.tsx
@@ -1,82 +1,82 @@
-import { useRecoilValueLoadable } from "recoil";
-import { allBlogs } from "../store/atoms";
-import { BlogCard, SearchInput } from "../components";
-import { BlogCardSkeleton } from "../components/skeletons";
+import { useRecoilValueLoadable } from 'recoil';
+import { allBlogs } from '../store/atoms';
+import { BlogCard, SearchInput } from '../components';
+import { BlogCardSkeleton } from '../components/skeletons';
 interface BlogI {
-	author: {
-		id: string;
-		email: string;
-		name: string;
-		password: string;
-	};
-	authorId: string;
-	labels: string[];
-	createdAt: Date;
-	id: string;
-	content: string;
-	image: string;
-	title: string;
-	published: boolean;
+    author: {
+        id: string;
+        email: string;
+        name: string;
+        password: string;
+    };
+    authorId: string;
+    labels: string[];
+    createdAt: Date;
+    id: string;
+    content: string;
+    image: string;
+    title: string;
+    published: boolean;
 }
 const Blogs = () => {
-	const arr = [1, 2, 3, 4, 5, 6];
-	const {
-		state,
-		contents: { data },
-	} = useRecoilValueLoadable(allBlogs(""));
-	if (state == "loading") {
-		return (
-			<section className="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
-				<div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative gap-4 h-full">
-					<div className="mb-4">
-						<SearchInput />
-					</div>
+    const arr = [1, 2, 3, 4, 5, 6];
+    const {
+        state,
+        contents: { data }
+    } = useRecoilValueLoadable(allBlogs(''));
+    if (state == 'loading') {
+        return (
+            <section className="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
+                <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative gap-4 h-full">
+                    <div className="mb-4">
+                        <SearchInput />
+                    </div>
 
-					<div className="flex items-center justify-center flex-col">
-						{arr.map((x) => {
-							return <BlogCardSkeleton key={x} />;
-						})}
-					</div>
-				</div>
-			</section>
-		);
-	}
+                    <div className="flex items-center justify-center flex-col">
+                        {arr.map((x) => {
+                            return <BlogCardSkeleton key={x} />;
+                        })}
+                    </div>
+                </div>
+            </section>
+        );
+    }
 
-	return (
-		<section className="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
-			<div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative  gap-4 h-full">
-				<div className="mb-4">
-					<SearchInput />
-				</div>
-				<div className="flex items-center justify-center text-center">
-					{data?.blogs.length === 0 && (
-						<p className="my-3 h-[50vh] text-lg text-gray-500 md:text-xl dark:text-gray-400">
-							No blogs found from the search result
-						</p>
-					)}
-				</div>
-				<div className="flex items-center justify-center flex-col">
-					{data?.blogs.map((blog: BlogI) => {
-						if (blog.published) {
-							return (
-								<BlogCard
-									key={blog.id}
-									title={blog.title}
-									content={blog.content}
-									id={blog.id}
-									createdAt={blog.createdAt}
-									image={blog.image}
-									author={blog.author.name}
-									labels={blog.labels}
-								/>
-							);
-						}
-					})}
-				</div>
-			</div>
-			<div className="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
-		</section>
-	);
+    return (
+        <section className="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative  gap-4 h-full">
+                <div className="mb-4">
+                    <SearchInput />
+                </div>
+                <div className="flex items-center justify-center text-center">
+                    {data?.blogs.length === 0 && (
+                        <p className="my-3 h-[50vh] text-lg text-gray-500 md:text-xl dark:text-gray-400">
+                            No blogs found from the search result
+                        </p>
+                    )}
+                </div>
+                <div className="flex items-center justify-center flex-col">
+                    {data?.blogs.map((blog: BlogI) => {
+                        if (blog.published) {
+                            return (
+                                <BlogCard
+                                    key={blog.id}
+                                    title={blog.title}
+                                    content={blog.content}
+                                    id={blog.id}
+                                    createdAt={blog.createdAt}
+                                    image={blog.image}
+                                    author={blog.author.name}
+                                    labels={blog.labels}
+                                />
+                            );
+                        }
+                    })}
+                </div>
+            </div>
+            <div className="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
+        </section>
+    );
 };
 
 export { Blogs };
--- client/src/store/atoms.ts
+++ client/src/store/atoms.ts
@@ -1,81 +1,81 @@
-import axios from "axios";
-import { atom, atomFamily, selector, selectorFamily } from "recoil";
+import axios from 'axios';
+import { atom, atomFamily, selector, selectorFamily } from 'recoil';
 const BASE_URL = import.meta.env.VITE_URL;
 const tokenAtom = atom({
-	key: "tokenAtom",
-	default: localStorage.getItem("token"),
+    key: 'tokenAtom',
+    default: localStorage.getItem('token')
 });
 const userIdAtom = atom({
-	key: "userIdAtom",
-	default: "",
+    key: 'userIdAtom',
+    default: ''
 });
 const userdetailsAtom = atom({
-	key: "userdetailsAtom",
-	default: selector({
-		key: "userdetailsSelector",
-		get: async ({ get }) => {
-			const Usertoken = get(tokenAtom);
-			const { data } = await axios.get(`${BASE_URL}/user/details`, {
-				headers: {
-					Authorization: "Bearer " + Usertoken,
-				},
-			});
-			return data.data.user;
-		},
-	}),
+    key: 'userdetailsAtom',
+    default: selector({
+        key: 'userdetailsSelector',
+        get: async ({ get }) => {
+            const Usertoken = get(tokenAtom);
+            const { data } = await axios.get(`${BASE_URL}/user/details`, {
+                headers: {
+                    Authorization: 'Bearer ' + Usertoken
+                }
+            });
+            return data.data.user;
+        }
+    })
 });
 const filterAtom = atom({
-	key: "filterAtom",
-	default: "",
+    key: 'filterAtom',
+    default: ''
 });
 const allBlogs = atomFamily({
-	key: "allBlogs",
-	default: selectorFamily({
-		key: "allBlogsSelector",
-		get:
-			() =>
-			async ({ get }) => {
-				const filter = get(filterAtom);
-				const { data } = await axios.get(
-					`${BASE_URL}/blog/bulk?filter=` + filter,
-				);
-				return data;
-			},
-	}),
+    key: 'allBlogs',
+    default: selectorFamily({
+        key: 'allBlogsSelector',
+        get:
+            () =>
+            async ({ get }) => {
+                const filter = get(filterAtom);
+                const { data } = await axios.get(
+                    `${BASE_URL}/blog/bulk?filter=` + filter
+                );
+                return data;
+            }
+    })
 });
 
 const userBlogs = atom({
-	key: "userBlogs",
-	default: selector({
-		key: "userBlogSelector",
-		get: async ({ get }) => {
-			const Usertoken = get(tokenAtom);
-			const { data } = await axios.get(`${BASE_URL}/blog/usrBlogs`, {
-				headers: {
-					Authorization: "Bearer " + Usertoken,
-				},
-			});
-			return data;
-		},
-	}),
+    key: 'userBlogs',
+    default: selector({
+        key: 'userBlogSelector',
+        get: async ({ get }) => {
+            const Usertoken = get(tokenAtom);
+            const { data } = await axios.get(`${BASE_URL}/blog/usrBlogs`, {
+                headers: {
+                    Authorization: 'Bearer ' + Usertoken
+                }
+            });
+            return data;
+        }
+    })
 });
 const particularBlog = atomFamily({
-	key: "particularBlog",
-	default: selectorFamily({
-		key: `particularBlog_$`,
-		get: (id: string) => async () => {
-			const { data } = await axios.get(`${BASE_URL}/blog/${id}`, {});
-			return data?.data?.blogs;
-		},
-	}),
+    key: 'particularBlog',
+    default: selectorFamily({
+        key: `particularBlog_$`,
+        get: (id: string) => async () => {
+            const { data } = await axios.get(`${BASE_URL}/blog/${id}`, {});
+            return data?.data?.blogs;
+        }
+    })
 });
 
 export {
-	allBlogs,
-	userIdAtom,
-	userBlogs,
-	particularBlog,
-	tokenAtom,
-	userdetailsAtom,
-	filterAtom,
+    allBlogs,
+    userIdAtom,
+    userBlogs,
+    particularBlog,
+    tokenAtom,
+    userdetailsAtom,
+    filterAtom
 };
